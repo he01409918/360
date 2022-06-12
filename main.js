fd7252ea5fa9321598bf5eba94153bb9200bbebd
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-06-12 19:54:36 +0800

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-pannellum.viewer('panorama-360-view', {
-    "type": "equirectangular",
-    "panorama": "https://i.ibb.co/M2wp6g2/mkizd8nk-a-room-filled-with-furniture-and-a-table.jpg",
-    "autoLoad": true
-})
